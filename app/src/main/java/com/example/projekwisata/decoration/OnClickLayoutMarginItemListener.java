f5f35a5c88ac687e4f0fc1208fbc8aaa36108fe8
wirasinaga <wira18ti@mahasiswa.pcr.ac.id>
2021-02-04 22:54:40 +0700

    home selesai

---
+++ app/src/main/java/com/example/projekwisata/decoration/OnClickLayoutMarginItemListener.java
@@ -0,0 +1,11 @@
+package com.example.projekwisata.decoration;
+
+import android.content.Context;
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+
+public interface OnClickLayoutMarginItemListener {
+    void onClick(Context context, View v, int position, int spanIndex, RecyclerView.State state);
+}
